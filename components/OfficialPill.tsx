ba6f5ba35c1c9a31874c75b15f011b20467cae85
Ebenezer Kwabena Blay Ackah <ebenezerkb.ackah@gmail.com>
2023-10-14 00:22:06 +0000

    standings

--- components/OfficialPill.tsx
+++ components/OfficialPill.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react'
 // import { GrAchievement } from 'react-icons/gr'
-import { FcPhoneAndroid } from 'react-icons/fc'
+import { FcAssistant, FcPhoneAndroid } from 'react-icons/fc'
 
 type Props = {
     row: any;
@@ -11,7 +11,7 @@ function OfficialPill({ row }: Props) {
   return (
     <Link href={`/official/${row.slug}`} key={row.slug} className="flex items-center space-x-1">
         {/* <GrAchievement className="w-5 h-5" /> */}
-        <FcPhoneAndroid className="w-5 h-5" />
+        <FcAssistant className="w-5 h-5" />
         <span className="text-xs font-semibold uppercase">{row.name}</span>
     </Link>
   )
