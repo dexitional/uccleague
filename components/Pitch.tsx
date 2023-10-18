c0ece46096728f5074e9333ae7f2d4e6839cc928
Ebenezer Kwabena Blay Ackah <ebenezerkb.ackah@gmail.com>
2023-10-18 19:58:42 +0000

    teams & park green

--- components/Pitch.tsx
+++ components/Pitch.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Pitch({children}: { children:React.ReactNode }) {
   return (
-    <div className="p-3 relative w-full aspect-[16/9]  bg-green-800 rounded border-2 border-green-100">
+    <div className="p-3 relative w-full h-fit aspect-[16/9]  bg-green-800 rounded-md border-2 border-green-300">
         <div className="absolute top-0 left-0 w-1/2 h-full border-green-100 border-r-2"></div>
 
         <div className="absolute top-[18%] left-0 w-1/5 h-3/5 border-green-100 border-l-0 border-2"></div>
