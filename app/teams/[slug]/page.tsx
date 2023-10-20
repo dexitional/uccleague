02e62164a0d5a465a5ef8e341bf62c80ebbb6296
Ebenezer Kwabena Blay Ackah <ebenezerkb.ackah@gmail.com>
2023-10-20 15:27:13 +0000

    teams & team profile

--- app/teams/[slug]/page.tsx
+++ app/teams/[slug]/page.tsx
@@ -51,7 +51,7 @@ async function Team({ params }: { params: { slug: string }}) {
                       {/* Team Setup or Tactics */}
                       { team.documents[0]?.teamTactics?.split(':')?.map((row:any,i:number) => (
                        <div key={i} className="flex flex-col items-center justify-between space-y-2 md:space-y-3">
-                          { row.split(' ').map((kitNo:any,j:number) => (
+                          { row.trim().split(' ').map((kitNo:any,j:number) => (
                           <div key={j} className="z-10 w-8 h-8 rounded-full bg-green-100 border-green-700 flex items-center justify-center">
                             <span className="font-bold text-gray-800">{kitNo}</span>
                           </div>
